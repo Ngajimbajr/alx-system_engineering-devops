160a705e8f60d45b1aa8eb9adc8ed2900995ff05
Ngajimbajr <ngajimbajr@gmail.com>
2023-12-26 12:00:44 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# puppet declarative script to install flask from pip3.
+# install_flask.pp
+
+# Ensure the 'pip' package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using 'pip3'
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
