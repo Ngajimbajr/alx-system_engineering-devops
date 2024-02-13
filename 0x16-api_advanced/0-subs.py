6e5a131ebc9fc489d29c4d435bcc32b57288b245
Ngajimbajr <ngajimbajr@gmail.com>
2024-02-13 13:20:01 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    url = ("https://api.reddit.com/r/{}/about.json". format(subreddit))
     headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
         return (0)
     response = response.json()
     if 'data' in response:
